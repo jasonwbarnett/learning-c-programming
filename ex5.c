cc795ba39b3d9d17d61e1e39b474b758e707b4f4
Jason Barnett <jason.w.barnett@gmail.com>
2013-08-11 23:26:53 -0500

    updated

--- ex5.c
+++ ex5.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /* This is a comment. */
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
   int distance = 100;
 
+  printf("hello %s.\n", argv[1]);
   // this is also a comment
   printf("You are %d miles away.\n", distance);
 
