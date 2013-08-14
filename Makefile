93e7c33af5c922fb96aacf0dd1ecf1360bcecba9
Jason Barnett <jason.w.barnett@gmail.com>
2013-08-14 11:09:49 -0500

    updates and fun stuff

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CFLAGS=-Wall
 
-all: ex3 ex4 ex5 ex6
+all: ex3 ex4 ex5 ex6 sine
 
 clean:
+	rm -f sine
 	find . -maxdepth 1 -name 'ex*' -a ! -name '*.c' -exec rm -Rf {} \;
--- ex6.c
+++ ex6.c
@@ -11,11 +11,17 @@ int main(int argc, char *argv[])
   char   *another     = "Bob";
 
   printf("this is the first byte: %c\n", first_name[0]);
-  printf("this is the first byte another: %s\n", another);
-  printf("int: %d, float: %d, double: %d, char: %d\n", sizeof(int), sizeof(float), sizeof(double), sizeof(char));
-  printf("int: %d, float: %f\n", distance, power);
+  printf("this is the string of another: %s\n", another);
+  printf("sizeof ::  int: %lu, float: %lu, double: %lu, char: %lu\n", sizeof(int), sizeof(float), sizeof(double), sizeof(char));
+  printf("int: %d, float: %0f\n", distance, power);
 
+  printf("You are %d miles away.\n", distance);
   printf("You have %f levels of power.\n", power);
   printf("You have %f awesome super powers.\n", super_power);
+  printf("I have an initial %c.\n", initial);
+  printf("I have a first name %s.\n", first_name);
+  printf("I have a last name %s.\n", last_name);
   printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
+
+  return 0;
 }
